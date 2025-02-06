d085a38a4b73ff34fc8b92fe1764f0b60b884b91
sujan2003 <niroulasujan7@gmail.com>
2025-02-06 14:54:40 -0600

    Files clean up and folders organization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Chat from './components/Chat'; //Importing Chat component(function) from component folder
+
+function App() {
+  return (
+    <div className="App">
+      <Chat/> {/* Running the Chat component */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Chat.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Chat() {
+  return (
+    <div>Here is where you can chat with out AI Agent!</div>
+  )
+}
