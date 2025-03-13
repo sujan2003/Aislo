fd07a3f93b9f67eaeffe8c58ae6a3ce803de0d17
sujan2003 <niroulasujan7@gmail.com>
2025-03-13 16:31:53 -0500

    the chat gets the response(recipe) based on the user input.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -22,23 +22,45 @@ const chatModel = new ChatOpenAI({
 })
 
 const prompt = ChatPromptTemplate.fromMessages([
-  ["system", "You are a world class food recipe documentation writer.\n You list the reciepe for {input}(food) in bullet point format. \n After the list for the {input}(food), you write a brief note to highlight the key points of the recipe or any additional information you think is important."],
+  ["system", "You are a world class food recipe documentation writer.\n You list the reciepe for {input}(food) in bullet point format. You constraints are you give any additional details about the recipe such as instructions and only list out recipe"],
+  //optional prompt: \n After the list for the {input}(food), you write a brief note to highlight the key points of the recipe or any additional information you think is important."
   ["user", "{input}"],
 ]);
 
 const chain = prompt.pipe(chatModel).pipe(outputParser);
 
-const response = await chain.invoke({
-  input: "Best Chocolate Chip Cookies",
-});
+// const response = await chain.invoke({
+//   input: "Best Chocolate Chip Cookies",
+// });
+
+
+const getRecipe = async (userInput)=> {
+  console.log("Fetching data...")
+  const response = await chain.invoke({
+    input: userInput,
+  });
+  return response;
+}
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Aislo chatbot backend server!');
 })
+// app.post('/api/data', (req, res) => {
+//   const inputData = req.body.input;
+//   const responseMessage = `Server received: ${inputData}`;
+//   res.json({ message: responseMessage });
+// });
 
-app.get('/response', (req, res) => {
-  res.send(response);
-})
+app.post('/api/data', async (req, res) => {
+  try {
+    const responseMessage = await getRecipe(req.body.input);
+    res.json({ message: responseMessage });
+  } catch(error){
+    console.error("Error fetching recipe: ", error);
+    res.status(500).json({message: "Error fetching recipe"})
+  }
+
+});
 
 app.listen(4000, () => {
   console.log('Backend server running on http://localhost:4000');
